e4912e178a40f27675cc1582b51b2c2566ba6ec7
Melevanina#87 <rebecalcon@hotmail.com>
2023-01-20 12:42:45 -0300

    Representado booleano de outra forma

--- tipos-variáveis/src/App.java
+++ tipos-variáveis/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+       
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = a==b ?"verdadeiro" : "falso"; 
+        
+
+            System.out.println(resultado);
+
     }
 }
