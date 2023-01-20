f15d6870387e7437cc91a9be8925610d1db6fd2b
Melevanina#87 <rebecalcon@hotmail.com>
2023-01-20 13:48:42 -0300

    testando

--- tipos-variáveis/src/App.java
+++ tipos-variáveis/src/App.java
@@ -11,4 +11,27 @@ public class App {
             System.out.println(resultado);
 
     }
+    public static void main(String[] args) {
+        String nomeUm = "Gleyson";
+        String nomeDois = new String ("Gleyson");
+        System.out.println(nomeUm.equals(nomeDois)); 
+
+
+
+    }
+
+    public static void main(String[] args){
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condiçoes são verdadeiras");
+        }
+if(condicao1 || condicao2){
+    System.out.println("Uma das condições é verdadeira");
+}
+
+System.out.println("fim");
+    }
+    
 }
